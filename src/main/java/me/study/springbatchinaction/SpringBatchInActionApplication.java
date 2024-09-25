db0109ba4d46be903b03b6dba407c91b14ed5d4d
geneaky <kaktus418@gmail.com>
2024-09-25 22:32:57 +0900

    init

---
+++ src/main/java/me/study/springbatchinaction/SpringBatchInActionApplication.java
@@ -0,0 +1,15 @@
+package me.study.springbatchinaction;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableBatchProcessing
+public class SpringBatchInActionApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBatchInActionApplication.class, args);
+  }
+
+}
